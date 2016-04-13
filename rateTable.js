ffeb78c329425de2ed7201fcda32fc6bb3a9a7bc
James Scott-Brown <james@jamesscottbrown.com>
2016-04-13 12:08:56 +0100

    Display rate-laws in table

--- rateTable.js
+++ rateTable.js
@@ -1,3 +1,14 @@
-/**
- * Created by jsb on 08/04/2016.
- */
+function fillRateTable() {
+    var url = path + "rateTable.html";
+    var http = new XMLHttpRequest();
+    http.open('get', url);
+    http.onreadystatechange = handleResponse;
+    http.send(null);
+
+    function handleResponse() {
+        if (http.readyState == 4) {
+            var response = http.responseText;
+            document.getElementById('modelKinetics').innerHTML=response;
+        }
+    }
+}
