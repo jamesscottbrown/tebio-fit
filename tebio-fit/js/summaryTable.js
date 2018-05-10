586c95363354112b4e6ec69ab4cd22cf9f2ad55c
James Scott-Brown <james@jamesscottbrown.com>
2018-05-10 19:42:13 +0100

    populate epsilon_schedule form rates file if automatic schedule used

--- tebio-fit/js/summaryTable.js
+++ tebio-fit/js/summaryTable.js
@@ -1,58 +1,43 @@
 function summaryTable() {
+    var table = d3.select('#summaryTable').append("table");
+    table.classed('table', true).classed('table-bordered', true); // apply bootstrap table themeing
 
-    var dataURL = path + "rates.txt";
+    var tHead = table.append("thead");
+    var tBody = table.append("tbody");
 
-    d3.text(dataURL, function (error, rawData) {
-        if (error) throw error;
+    var columns = [{"table_column": 1, "display_name": "Epsilon"},
+        {"table_column": 2, "display_name": "Simulations" },
+        {"table_column": 3, "display_name": "Acceptance Rate"},
+        {"table_column": 4, "display_name": "Simulation Time/s" }];
+
+    tHead.append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+        .text(function (column) {
+            return column.display_name;
+        });
 
 
-        rawData = rawData.replace(/\[/g, '').replace(/]/g, '').replace(/  /g, ' ');
+    var rows = tBody.selectAll("tr")
+        .data(summary_model_data)
+        .enter()
+        .append("tr");
 
-        var parsedData = d3.dsv(" ", "text/plain")
-            .parseRows(rawData);
+    rows.selectAll("td")
+        .data(function (row) {
+            // callback must return data in format:
+            // {name: "p5", type: "uniform", min: 0, max: 30}
 
-        var table = d3.select('#summaryTable').append("table");
-        table.classed('table', true).classed('table-bordered', true); // apply bootstrap table themeing
-
-        var tHead = table.append("thead");
-        var tBody = table.append("tbody");
-
-        var columns = [{"table_column": 1, "display_name": "Epsilon"},
-            {"table_column": 2, "display_name": "Simulations" },
-            {"table_column": 3, "display_name": "Acceptance Rate"},
-            {"table_column": 4, "display_name": "Simulation Time/s" }];
-
-        tHead.append("tr")
-            .selectAll("th")
-            .data(columns)
-            .enter()
-            .append("th")
-            .text(function (column) {
-                return column.display_name;
+            return columns.map(function (column) {
+                return {column: column.display_name, value: row[column.table_column]};
             });
 
-
-        var rows = tBody.selectAll("tr")
-            .data(parsedData)
-            .enter()
-            .append("tr");
-
-        rows.selectAll("td")
-            .data(function (row) {
-                // callback must return data in format:
-                // {name: "p5", type: "uniform", min: 0, max: 30}
-
-                return columns.map(function (column) {
-                    return {column: column.display_name, value: row[column.table_column]};
-                });
-
-            })
-            .enter()
-            .append("td")
-            .html(function (d) {
-                return d.value;
-            });
-
-    });
-
+        })
+        .enter()
+        .append("td")
+        .html(function (d) {
+            return d.value;
+        });
 }
